0b50283a43e8e7b40ec9c8c0ab1aef1fbd094dab
Chris Olivier <cjolivier01@apache.org>
2024-12-22 19:28:24 -0800

    stuff

--- src/libs/kmeans/cuda_io.cu
+++ src/libs/kmeans/cuda_io.cu
@@ -15,169 +15,175 @@
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>     /* strtok() */
-#include <sys/types.h>  /* open() */
+#include <string.h> /* strtok() */
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>     /* read(), close() */
+#include <sys/types.h> /* open() */
+#include <unistd.h> /* read(), close() */
 
 #include "kmeans.h"
 
 #define MAX_CHAR_PER_LINE 128
 
+namespace hm {
+namespace kmeans {
 
 /*---< file_read() >---------------------------------------------------------*/
-float** file_read(int   isBinaryFile,  /* flag: 0 or 1 */
-                  char *filename,      /* input file name */
-                  int  *numObjs,       /* no. data objects (local) */
-                  int  *numCoords)     /* no. coordinates */
+float** file_read(
+    int isBinaryFile, /* flag: 0 or 1 */
+    char* filename, /* input file name */
+    int* numObjs, /* no. data objects (local) */
+    int* numCoords) /* no. coordinates */
 {
-    float **objects;
-    int     i, j, len;
-    ssize_t numBytesRead;
+  float** objects;
+  int i, j, len;
+  ssize_t numBytesRead;
 
-    if (isBinaryFile) {  /* input file is in raw binary format -------------*/
-        int infile;
-        if ((infile = open(filename, O_RDONLY, "0600")) == -1) {
-            fprintf(stderr, "Error: no such file (%s)\n", filename);
-            return NULL;
-        }
-        numBytesRead = read(infile, numObjs,    sizeof(int));
-        assert(numBytesRead == sizeof(int));
-        numBytesRead = read(infile, numCoords, sizeof(int));
-        assert(numBytesRead == sizeof(int));
-        if (_debug) {
-            printf("File %s numObjs   = %d\n",filename,*numObjs);
-            printf("File %s numCoords = %d\n",filename,*numCoords);
-        }
-
-        /* allocate space for objects[][] and read all objects */
-        len = (*numObjs) * (*numCoords);
-        objects    = (float**)malloc((*numObjs) * sizeof(float*));
-        assert(objects != NULL);
-        objects[0] = (float*) malloc(len * sizeof(float));
-        assert(objects[0] != NULL);
-        for (i=1; i<(*numObjs); i++)
-            objects[i] = objects[i-1] + (*numCoords);
-
-        numBytesRead = read(infile, objects[0], len*sizeof(float));
-        assert(numBytesRead == len*sizeof(float));
-
-        close(infile);
+  if (isBinaryFile) { /* input file is in raw binary format -------------*/
+    int infile;
+    if ((infile = open(filename, O_RDONLY, "0600")) == -1) {
+      fprintf(stderr, "Error: no such file (%s)\n", filename);
+      return NULL;
     }
-    else {  /* input file is in ASCII format -------------------------------*/
-        FILE *infile;
-        char *line, *ret;
-        int   lineLen;
+    numBytesRead = read(infile, numObjs, sizeof(int));
+    assert(numBytesRead == sizeof(int));
+    numBytesRead = read(infile, numCoords, sizeof(int));
+    assert(numBytesRead == sizeof(int));
+    if (_debug) {
+      printf("File %s numObjs   = %d\n", filename, *numObjs);
+      printf("File %s numCoords = %d\n", filename, *numCoords);
+    }
 
-        if ((infile = fopen(filename, "r")) == NULL) {
-            fprintf(stderr, "Error: no such file (%s)\n", filename);
-            return NULL;
-        }
+    /* allocate space for objects[][] and read all objects */
+    len = (*numObjs) * (*numCoords);
+    objects = (float**)malloc((*numObjs) * sizeof(float*));
+    assert(objects != NULL);
+    objects[0] = (float*)malloc(len * sizeof(float));
+    assert(objects[0] != NULL);
+    for (i = 1; i < (*numObjs); i++)
+      objects[i] = objects[i - 1] + (*numCoords);
 
-        /* first find the number of objects */
-        lineLen = MAX_CHAR_PER_LINE;
-        line = (char*) malloc(lineLen);
+    numBytesRead = read(infile, objects[0], len * sizeof(float));
+    assert(numBytesRead == len * sizeof(float));
+
+    close(infile);
+  } else { /* input file is in ASCII format -------------------------------*/
+    FILE* infile;
+    char *line, *ret;
+    int lineLen;
+
+    if ((infile = fopen(filename, "r")) == NULL) {
+      fprintf(stderr, "Error: no such file (%s)\n", filename);
+      return NULL;
+    }
+
+    /* first find the number of objects */
+    lineLen = MAX_CHAR_PER_LINE;
+    line = (char*)malloc(lineLen);
+    assert(line != NULL);
+
+    (*numObjs) = 0;
+    while (fgets(line, lineLen, infile) != NULL) {
+      /* check each line to find the max line length */
+      while (strlen(line) == lineLen - 1) {
+        /* this line read is not complete */
+        len = strlen(line);
+        fseek(infile, -len, SEEK_CUR);
+
+        /* increase lineLen */
+        lineLen += MAX_CHAR_PER_LINE;
+        line = (char*)realloc(line, lineLen);
         assert(line != NULL);
 
-        (*numObjs) = 0;
-        while (fgets(line, lineLen, infile) != NULL) {
-            /* check each line to find the max line length */
-            while (strlen(line) == lineLen-1) {
-                /* this line read is not complete */
-                len = strlen(line);
-                fseek(infile, -len, SEEK_CUR);
+        ret = fgets(line, lineLen, infile);
+        assert(ret != NULL);
+      }
 
-                /* increase lineLen */
-                lineLen += MAX_CHAR_PER_LINE;
-                line = (char*) realloc(line, lineLen);
-                assert(line != NULL);
+      if (strtok(line, " \t\n") != 0)
+        (*numObjs)++;
+    }
+    rewind(infile);
+    if (_debug)
+      printf("lineLen = %d\n", lineLen);
 
-                ret = fgets(line, lineLen, infile);
-                assert(ret != NULL);
-            }
-
-            if (strtok(line, " \t\n") != 0)
-                (*numObjs)++;
-        }
-        rewind(infile);
-        if (_debug) printf("lineLen = %d\n",lineLen);
-
-        /* find the no. objects of each object */
-        (*numCoords) = 0;
-        while (fgets(line, lineLen, infile) != NULL) {
-            if (strtok(line, " \t\n") != 0) {
-                /* ignore the id (first coordiinate): numCoords = 1; */
-                while (strtok(NULL, " ,\t\n") != NULL) (*numCoords)++;
-                break; /* this makes read from 1st object */
-            }
-        }
-        rewind(infile);
-        if (_debug) {
-            printf("File %s numObjs   = %d\n",filename,*numObjs);
-            printf("File %s numCoords = %d\n",filename,*numCoords);
-        }
-
-        /* allocate space for objects[][] and read all objects */
-        len = (*numObjs) * (*numCoords);
-        objects    = (float**)malloc((*numObjs) * sizeof(float*));
-        assert(objects != NULL);
-        objects[0] = (float*) malloc(len * sizeof(float));
-        assert(objects[0] != NULL);
-        for (i=1; i<(*numObjs); i++)
-            objects[i] = objects[i-1] + (*numCoords);
-
-        i = 0;
-        /* read all objects */
-        while (fgets(line, lineLen, infile) != NULL) {
-            if (strtok(line, " \t\n") == NULL) continue;
-            for (j=0; j<(*numCoords); j++)
-                objects[i][j] = atof(strtok(NULL, " ,\t\n"));
-            i++;
-        }
-
-        fclose(infile);
-        free(line);
+    /* find the no. objects of each object */
+    (*numCoords) = 0;
+    while (fgets(line, lineLen, infile) != NULL) {
+      if (strtok(line, " \t\n") != 0) {
+        /* ignore the id (first coordiinate): numCoords = 1; */
+        while (strtok(NULL, " ,\t\n") != NULL)
+          (*numCoords)++;
+        break; /* this makes read from 1st object */
+      }
+    }
+    rewind(infile);
+    if (_debug) {
+      printf("File %s numObjs   = %d\n", filename, *numObjs);
+      printf("File %s numCoords = %d\n", filename, *numCoords);
     }
 
-    return objects;
+    /* allocate space for objects[][] and read all objects */
+    len = (*numObjs) * (*numCoords);
+    objects = (float**)malloc((*numObjs) * sizeof(float*));
+    assert(objects != NULL);
+    objects[0] = (float*)malloc(len * sizeof(float));
+    assert(objects[0] != NULL);
+    for (i = 1; i < (*numObjs); i++)
+      objects[i] = objects[i - 1] + (*numCoords);
+
+    i = 0;
+    /* read all objects */
+    while (fgets(line, lineLen, infile) != NULL) {
+      if (strtok(line, " \t\n") == NULL)
+        continue;
+      for (j = 0; j < (*numCoords); j++)
+        objects[i][j] = atof(strtok(NULL, " ,\t\n"));
+      i++;
+    }
+
+    fclose(infile);
+    free(line);
+  }
+
+  return objects;
 }
 
 /*---< file_write() >---------------------------------------------------------*/
-int file_write(char      *filename,     /* input file name */
-               int        numClusters,  /* no. clusters */
-               int        numObjs,      /* no. data objects */
-               int        numCoords,    /* no. coordinates (local) */
-               float    **clusters,     /* [numClusters][numCoords] centers */
-               int       *membership)   /* [numObjs] */
+int file_write(
+    char* filename, /* input file name */
+    int numClusters, /* no. clusters */
+    int numObjs, /* no. data objects */
+    int numCoords, /* no. coordinates (local) */
+    float** clusters, /* [numClusters][numCoords] centers */
+    int* membership) /* [numObjs] */
 {
-    FILE *fptr;
-    int   i, j;
-    char  outFileName[1024];
+  FILE* fptr;
+  int i, j;
+  char outFileName[1024];
 
-    /* output: the coordinates of the cluster centres ----------------------*/
-    sprintf(outFileName, "%s.cluster_centres", filename);
-    printf("Writing coordinates of K=%d cluster centers to file \"%s\"\n",
-           numClusters, outFileName);
-    fptr = fopen(outFileName, "w");
-    for (i=0; i<numClusters; i++) {
-        fprintf(fptr, "%d ", i);
-        for (j=0; j<numCoords; j++)
-            fprintf(fptr, "%f ", clusters[i][j]);
-        fprintf(fptr, "\n");
-    }
-    fclose(fptr);
+  /* output: the coordinates of the cluster centres ----------------------*/
+  sprintf(outFileName, "%s.cluster_centres", filename);
+  printf("Writing coordinates of K=%d cluster centers to file \"%s\"\n", numClusters, outFileName);
+  fptr = fopen(outFileName, "w");
+  for (i = 0; i < numClusters; i++) {
+    fprintf(fptr, "%d ", i);
+    for (j = 0; j < numCoords; j++)
+      fprintf(fptr, "%f ", clusters[i][j]);
+    fprintf(fptr, "\n");
+  }
+  fclose(fptr);
 
-    /* output: the closest cluster centre to each of the data points --------*/
-    sprintf(outFileName, "%s.membership", filename);
-    printf("Writing membership of N=%d data objects to file \"%s\"\n",
-           numObjs, outFileName);
-    fptr = fopen(outFileName, "w");
-    for (i=0; i<numObjs; i++)
-        fprintf(fptr, "%d %d\n", i, membership[i]);
-    fclose(fptr);
+  /* output: the closest cluster centre to each of the data points --------*/
+  sprintf(outFileName, "%s.membership", filename);
+  printf("Writing membership of N=%d data objects to file \"%s\"\n", numObjs, outFileName);
+  fptr = fopen(outFileName, "w");
+  for (i = 0; i < numObjs; i++)
+    fprintf(fptr, "%d %d\n", i, membership[i]);
+  fclose(fptr);
 
-    return 1;
+  return 1;
 }
+} // namespace kmeans
+} // namespace hm
--- src/libs/kmeans/wtime.c
+++ src/libs/kmeans/wtime.c
@@ -9,32 +9,36 @@
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-double wtime(void) 
-{
-    double          now_time;
-    struct timeval  etstart;
-    struct timezone tzp;
+namespace hm {
+namespace kmeans {
 
-    if (gettimeofday(&etstart, &tzp) == -1)
-        perror("Error: calling gettimeofday() not successful.\n");
+double wtime(void) {
+  double now_time;
+  struct timeval etstart;
+  struct timezone tzp;
 
-    now_time = ((double)etstart.tv_sec) +              /* in seconds */
-               ((double)etstart.tv_usec) / 1000000.0;  /* in microseconds */
-    return now_time;
+  if (gettimeofday(&etstart, &tzp) == -1)
+    perror("Error: calling gettimeofday() not successful.\n");
+
+  now_time = ((double)etstart.tv_sec) + /* in seconds */
+      ((double)etstart.tv_usec) / 1000000.0; /* in microseconds */
+  return now_time;
 }
 
 #ifdef _TESTING_
-int main(int argc, char **argv) {
-    double time;
+int main(int argc, char** argv) {
+  double time;
 
-    time = wtime();
-    printf("time of day = %10.4f\n", time);
+  time = wtime();
+  printf("time of day = %10.4f\n", time);
 
-    return 0;
+  return 0;
 }
 #endif
 
+} // namespace kmeans
+} // namespace hm
