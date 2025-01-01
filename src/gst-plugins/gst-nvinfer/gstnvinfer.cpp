1f4facfa3956be328085c9392788b6813c31771b
Chris Olivier <cjolivier01@apache.org>
2025-01-01 07:41:18 -0800

    ..

--- src/gst-plugins/gst-nvinfer/gstnvinfer.cpp
+++ src/gst-plugins/gst-nvinfer/gstnvinfer.cpp
@@ -2430,6 +2430,10 @@ static GstFlowReturn gst_nvinfer_submit_input_buffer(
   GstFlowReturn flow_ret;
   std::string nvtx_str;
 
+  g_print("gst_nvinfer_submit_input_buffer\n");
+  fflush(stdout);
+
+
   /* Check for model updates and replace the model if a new model is loaded. */
   if (impl->ensureReplaceNextContext() != NVDSINFER_SUCCESS) {
     GST_ELEMENT_ERROR(
