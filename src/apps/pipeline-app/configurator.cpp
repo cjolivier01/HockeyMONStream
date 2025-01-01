f998925f7405d26fe9d842014b1e3f3bdb72ff41
Chris Olivier <cjolivier01@apache.org>
2025-01-01 07:54:54 -0800

    ..

--- src/apps/pipeline-app/configurator.cpp
+++ src/apps/pipeline-app/configurator.cpp
@@ -1,11 +1,20 @@
 #include "apps/pipeline-app/configurator.h"
 namespace hm {
-Configurator::Configurator() {
+Configurator::Configurator(
+    const std::string& game_id,
+    const std::string& game_root_dir,
+    const std::string& config_root_dir)
+    : game_id_(game_id), config_root_dir_(config_root_dir), game_root_dir_(game_root_dir) {
   // Constructor
 }
 Configurator::~Configurator() {
   // Destructor
 }
+
+YAML::Node& Configurator::amend_config(const YAML::Node& new_config, YAML::Node& base_config) {
+  return base_config;
+}
+
 void Configurator::configure() {
   // Configure
 }
--- src/apps/pipeline-app/configurator.h
+++ src/apps/pipeline-app/configurator.h
@@ -1,9 +1,24 @@
 #pragma once
+
+#include "ConfigYaml.h"
+
+#include <string>
+
 namespace hm {
 class Configurator {
  public:
-  Configurator();
+  Configurator(const std::string& game_id, const std::string& game_root_dir, const std::string& config_root_dir);
   virtual ~Configurator();
   void configure();
+
+ private:
+  YAML::Node& amend_config(const YAML::Node& new_config, YAML::Node& base_config);
+
+  const std::string game_id_;
+  const std::string config_root_dir_;
+  const std::string game_root_dir_;
+
+  // The fully-realzied merged config
+  const YAML::Node config_;
 };
 } // namespace hm
