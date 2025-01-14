259d23cadafbc4c1b085a5cd34924c3183e7f1f7
Christopher Olivier <colivier@tesla.com>
2025-01-14 08:55:23 -0800

    ..

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
-pushd external/deepstream/sources/libs
-CUDA_VERSION="$(nvcc --version | grep "release" | sed 's/,/ /g' | awk '{print$5}')"
-CUDA_VER="${CUDA_VERSION}" make -k -j $(nproc) $@
-popd
-pushd external/deepstream/sources/gst-plugins
-CUDA_VER="${CUDA_VERSION}" ./build_all.sh
-popd
+make -j $(nproc) -C external/deepstream
+
