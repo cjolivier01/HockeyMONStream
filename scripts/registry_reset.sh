ff8870f14fa97a53e2bf5464b9b886462930f03a
Christopher Olivier <cjolivier01@gmail.com>
2025-01-27 20:16:05 -0800

    ..

--- scripts/registry_reset.sh
+++ scripts/registry_reset.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+rm -f .registry-synced
 pkill gxf_server
-registry cache -c
+/opt/nvidia/graph-composer/registry cache -c
 rm -rf /var/tmp/gxf/default_repo/*
-registry repo sync -n ngc-public
-registry repo sync -n default
+set -e
+/opt/nvidia/graph-composer/registry repo sync -n ngc-public
+/opt/nvidia/graph-composer/registry repo sync -n default
+touch .registry-synced
