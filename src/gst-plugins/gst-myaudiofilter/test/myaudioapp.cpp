aa84b86123cd2ecb91d7690ad8323d48087d6088
Chris Olivier <cjolivier01@apache.org>
2025-02-02 09:50:31 -0800

    ...

--- src/gst-plugins/gst-myaudiofilter/test/myaudioapp.cpp
+++ src/gst-plugins/gst-myaudiofilter/test/myaudioapp.cpp
@@ -88,40 +88,6 @@ static void on_demuxer_pad_added(GstElement* element, GstPad* pad, gpointer data
   gst_caps_unref(caps);
 }
 
-static void cb_newpad_audio(GstElement* decodebin, GstPad* pad, gpointer data) {
-  GstCaps* caps = gst_pad_query_caps(pad, NULL);
-  const GstStructure* str = gst_caps_get_structure(caps, 0);
-  const gchar* name = gst_structure_get_name(str);
-
-  if (g_str_has_prefix(name, "audio/x-raw")) {
-    NvDsSrcBin* bin = (NvDsSrcBin*)data;
-
-    GstPad* sinkpad = gst_element_get_static_pad(bin->audio_converter, "sink");
-
-    if (gst_pad_link(pad, sinkpad) != GST_PAD_LINK_OK)
-      NVGSTDS_ERR_MSG_V("Failed to link decodebin to pipeline");
-    gst_object_unref(sinkpad);
-  } else if (!strncmp(name, "video", 5)) {
-    /** connect video to fakesink and ignore the same */
-    NvDsSrcBin* bin = (NvDsSrcBin*)data;
-    bin->fakesink = gst_element_factory_make("fakesink", "src_fakesink");
-    if (!bin->fakesink) {
-      NVGSTDS_ERR_MSG_V("Could not create 'src_fakesink' for video path");
-      return;
-    }
-
-    g_object_set(G_OBJECT(bin->fakesink), "sync", FALSE, "async", FALSE, NULL);
-    gst_bin_add_many(GST_BIN(bin->bin), bin->fakesink, NULL);
-
-    GstPad* sinkpad = gst_element_get_static_pad(bin->fakesink, "sink");
-
-    if (gst_pad_link(pad, sinkpad) != GST_PAD_LINK_OK)
-      NVGSTDS_ERR_MSG_V("Failed to link decodebin to pipeline");
-    gst_object_unref(sinkpad);
-  }
-  gst_caps_unref(caps);
-}
-
 gint main(gint argc, gchar* argv[]) {
   GstStateChangeReturn ret;
   GstElement *pipeline, *filesrc, *demuxer{nullptr}, *decoder{nullptr}, *filter, *sink;
@@ -147,19 +113,16 @@ gint main(gint argc, gchar* argv[]) {
   watch_id = gst_bus_add_watch(bus, bus_call, loop);
   gst_object_unref(bus);
 
-#if 0
-  std::string uri = "file://";
-  uri += std::string(argv[1]);
-  filesrc = gst_element_factory_make("uridecodebin", "my_urisource");
-  g_object_set(filesrc, "uri", uri.c_str(), NULL);
-  // g_signal_connect(G_OBJECT(filesrc), "pad-added", G_CALLBACK(cb_newpad_audio), /*bin*/ nullptr);
-#else
-  filesrc = gst_element_factory_make("filesrc", "my_filesource");
-  g_object_set(G_OBJECT(filesrc), "location", argv[1], NULL);
-  demuxer = gst_element_factory_make("qtdemux", "my_demuxer");
-  decoder = gst_element_factory_make("decodebin", "decodebin");
-  g_signal_connect(demuxer, "pad-added", G_CALLBACK(on_demuxer_pad_added), decoder);
-#endif
+  if (g_strrstr(argv[1], "file:/")) {
+    filesrc = gst_element_factory_make("uridecodebin", "my_urisource");
+    g_object_set(filesrc, "uri", argv[1], NULL);
+  } else {
+    filesrc = gst_element_factory_make("filesrc", "my_filesource");
+    g_object_set(G_OBJECT(filesrc), "location", argv[1], NULL);
+    demuxer = gst_element_factory_make("qtdemux", "my_demuxer");
+    decoder = gst_element_factory_make("decodebin", "decodebin");
+    g_signal_connect(demuxer, "pad-added", G_CALLBACK(on_demuxer_pad_added), decoder);
+  }
 
   /* putting an audioconvert element here to convert the output of the
    * decoder into a format that myaudiofilter can handle (we are assuming it
