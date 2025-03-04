550b12f7feebae042098a6b0858f920cee93055e
Christopher Olivier <cjolivier01@gmail.com>
2025-03-03 18:26:14 -0800

    ..

--- src/libs/stitching/ConfigureStitching.cpp
+++ src/libs/stitching/ConfigureStitching.cpp
@@ -2,11 +2,25 @@
 
 namespace hm {
 namespace stitching {
+
+namespace {
+//
+}
+
+absl::StatusOr<bool> is_stitching_configured(const std::string& game_dir) {
+  return false;
+}
+
+bool can_configure_stitching(const YAML::Node& config) {
+  return true;
+}
+
 absl::Status configure_stitching(
     const std::string& game_id,
     surface::Surface left_surface,
     surface::Surface right_surface) {
   return absl::OkStatus();
 }
+
 } // namespace stitching
 } // namespace hm
--- src/libs/stitching/ConfigureStitching.h
+++ src/libs/stitching/ConfigureStitching.h
@@ -3,18 +3,21 @@
 #include "hstream/libs/common/Surface.h"
 
 #include "absl/status/statusor.h"
+#include "yaml-cpp/yaml.h"
 
 #include <string>
 
 namespace hm {
 namespace stitching {
 
-absl::StatusOr<bool> is_stitching_configured(const std::string& game_id);
+absl::StatusOr<bool> is_stitching_configured(const std::string& game_dir);
+
+bool can_configure_stitching(const YAML::Node& config);
 
 absl::Status configure_stitching(
     const std::string& game_id,
     surface::Surface left_surface,
     surface::Surface right_surface);
 
-}
+} // namespace stitching
 } // namespace hm
