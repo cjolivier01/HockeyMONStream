59402f7ce98222d10b78a6f9406469e161f5a4d0
Chris Olivier <cjolivier01@apache.org>
2025-03-24 18:07:39 -0700

    ..

--- src/libs/camera/AutoFocus.cpp
+++ src/libs/camera/AutoFocus.cpp
@@ -293,7 +293,7 @@ absl::Status auto_focus_cameras(
     const CameraConnection& camera = cameras[i];
     threads.at(i) = std::make_unique<std::thread>(
         TO_STRING("Camera " << camera.sensor_id << "-" << camera.i2c_bus),
-        [index = i, &statuses, show, interactive, verbose, force]() {
+        [index = i, &camera, &statuses, show, interactive, verbose, force]() {
           statuses.at(index) = auto_focus_csi_camera(
               camera.sensor_id,
               camera.i2c_bus,
