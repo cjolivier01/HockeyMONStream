ef1d3372ae59d555f1e41b78662fef8674994b14
Christopher Olivier <cjolivier01@gmail.com>
2025-04-11 16:50:00 -0700

    ...

--- src/gst-plugins/gst-videoprep/playcropper/playcropper.cpp
+++ src/gst-plugins/gst-videoprep/playcropper/playcropper.cpp
@@ -17,6 +17,7 @@
 #include "hstream/src/gst-plugins/gst-playtracker/PlayTrackerCtx.h"
 #include "hstream/src/gst-plugins/gst-videoprep/algorithm-base/preputils.h"
 #include "hstream/src/gst-plugins/gst-videoprep/playcropper/cudaPlayCropper.h"
+#include "hstream/src/gst-plugins/gst-videoprep/playtracker/playtracker_payload.h"
 #include "hstream/src/libs/common/Status.h"
 #include "hstream/src/libs/draw_display/DrawDisplayMeta.h"
 #include "hstream/src/libs/draw_display/Fonts.h"
@@ -236,15 +237,16 @@ absl::Status PlayCropperPriv::GenerateOutput(
     tbox.top *= scale_h;
     tbox.bottom *= scale_h;
 
+    // Calculate rotation angle
+    float angle = 0.0f;
+
     // FloatValue width_for_ratio = 1.0;
-    // const playtracker::PlayTrackerPayload* ptpayload =
-    //     playtracker::PlayTrackerPayload::get_payload<playtracker::PlayTrackerPayload>(frame_meta);
+    const playtracker::PlayTrackerPayload* ptpayload =
+        playtracker::PlayTrackerPayload::get_payload<playtracker::PlayTrackerPayload>(frame_meta);
     // if (ptpayload) {
     //   width_for_ratio = FloatValue(ptpayload->arena_box().width()) / frame_meta->source_frame_width;
     // }
 
-    // Calculate rotation angle
-    float angle = 0.0f;
     const float max_angle = fixed_edge_rotation_angle_;
     const float half_width = float(frame_meta->source_frame_width) / 2;
     const float tcx = tbox.center().x;
