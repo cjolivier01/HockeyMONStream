5cfe193dc42907d3821b3a04771c495948e1cd7d
Christopher Olivier <colivier@tesla.com>
2025-05-09 10:36:48 -0700

    ...

--- src/libs/stitching/ConfigureStitching.h
+++ src/libs/stitching/ConfigureStitching.h
@@ -1,11 +1,8 @@
 #pragma once
 
-#include "hstream/src/libs/common/Surface.h"
-
-#include "absl/status/statusor.h"
-#include "yaml-cpp/yaml.h"
-
 #include <string>
+#include "absl/status/statusor.h"
+#include "hstream/src/libs/common/Surface.h"
 
 namespace hm {
 namespace stitching {
