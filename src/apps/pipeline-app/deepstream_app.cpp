ae794813d7d7ea82b3986c510d841abe02ba36e3
Chris Olivier <cjolivier01@apache.org>
2025-07-20 08:31:48 -0700

    ..

--- src/apps/pipeline-app/deepstream_app.cpp
+++ src/apps/pipeline-app/deepstream_app.cpp
@@ -12,7 +12,8 @@
 #include <cstddef>
 
 #include <gst/gstelementfactory.h>
-// #include "gst-nvdscustommessage.h"
+#include <gst-nvdscommonconfig.h>
+#include <gst-nvdscustommessage.h>
 
 #define MAX_DISPLAY_LEN 64
 static guint demux_batch_num = 0;
