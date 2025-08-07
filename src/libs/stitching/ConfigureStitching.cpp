8e184b2f78dc76e865a6d80e61fe67def8369448
Christopher Olivier <cjolivier01@gmail.com>
2025-08-07 13:24:12 -0700

    ...

--- src/libs/stitching/ConfigureStitching.cpp
+++ src/libs/stitching/ConfigureStitching.cpp
@@ -402,8 +402,7 @@ absl::Status create_control_points(
   size_t max_control_points = utils::getenv("HM_MAX_CONTROL_POINTS", 500UL);
 
   std::vector<std::string> cmd{
-      get_python_interp(),
-      "create_control_points.py",
+      "hmcreate_control_points",
       "--left",
       left_file,
       "--right",
