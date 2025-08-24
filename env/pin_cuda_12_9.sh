5460bc40898b1a4c33fa65694d2768cf7978d9c8
Chris Olivier <cjolivier01@apache.org>
2025-08-24 08:46:13 -0700

    ..

---
+++ env/pin_cuda_12_9.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt install \
+  tensorrt-dev=10.13.2.6-1+cuda12.9 \
+  libnvinfer-dev=10.13.2.6-1+cuda12.9 \
+  libnvinfer-lean-dev=10.13.2.6-1+cuda12.9 \
+  libnvinfer-dispatch-dev=10.13.2.6-1+cuda12.9 \
+  libnvinfer-plugin-dev=10.13.2.6-1+cuda12.9 \
+  libnvinfer-vc-plugin-dev=10.13.2.6-1+cuda12.9 \
+  libnvonnxparsers-dev=10.13.2.6-1+cuda12.9 \
+  libnvinfer-win-builder-resource10=10.13.2.6-1+cuda12.9 \
+  libnvinfer-headers-python-plugin-dev=10.13.2.6-1+cuda12.9 \
+  libnvinfer-headers-dev=10.13.2.6-1+cuda12.9 \
+  libnvinfer10=10.13.2.6-1+cuda12.9 \
+  libnvinfer-dispatch10=10.13.2.6-1+cuda12.9 \
+  libnvinfer-headers-plugin-dev=10.13.2.6-1+cuda12.9 \
+  libnvinfer-plugin10=10.13.2.6-1+cuda12.9 \
+  libnvinfer-vc-plugin10=10.13.2.6-1+cuda12.9 \
+  libnvinfer-lean10=10.13.2.6-1+cuda12.9 \
+  libnvonnxparsers10=10.13.2.6-1+cuda12.9
